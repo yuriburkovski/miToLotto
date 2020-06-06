721f24f3e8832cbd76ee4b4c5614396775e12a21
yuriburkovski <yuriiburkovskyi@gmail.com>
2020-06-06 14:24:10 +0200

    added hibernate module

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,21 @@
 package org.example;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
-public class App 
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static SessionFactory factory;
+
+    public static void main( String[] args ){
+
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
     }
 }
